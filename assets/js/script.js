17b5cf01adbeb5b9474ed57439026153c72674c6
Santiago <santi.ari.pani@outlook.com>
2021-12-01 18:24:45 +0100

    valoracionesyestiloinicial

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,4 +34,58 @@ $(function () {
         ratedFill: 'green',
         readOnly: true
       });
+      $("#val1").rateYo({
+        rating: 5,
+        spacing: '5px',
+        ratedFill: 'green',
+        readOnly: true
+      });
+      $("#val2").rateYo({
+        rating: 3,
+        spacing: '5px',
+        ratedFill: 'yellow',
+        readOnly: true
+      });
+      $("#val3").rateYo({
+        rating: 4,
+        spacing: '5px',
+        ratedFill: 'springgreen',
+        readOnly: true
+      });
+      $("#val4").rateYo({
+        rating: 5,
+        spacing: '5px',
+        ratedFill: 'springgreen',
+        readOnly: true
+      });
+      $("#val5").rateYo({
+        rating: 4,
+        spacing: '5px',
+        ratedFill: 'springgreen',
+        readOnly: true
+      });
+      $("#val6").rateYo({
+        rating: 2,
+        spacing: '5px',
+        ratedFill: 'red',
+        readOnly: true
+      });
+      $("#val7").rateYo({
+        rating: 4,
+        spacing: '5px',
+        ratedFill: 'springgreen',
+        readOnly: true
+      });
+      $("#val8").rateYo({
+        rating: 5,
+        spacing: '5px',
+        ratedFill: 'green',
+        readOnly: true
+      });
+      $("#val9").rateYo({
+        rating: 4,
+        spacing: '5px',
+        ratedFill: 'springgreen',
+        readOnly: true
+      });
   });
