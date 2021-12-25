7c0469b369d4b67ab86ec09e3cdac8b30b5e93fe
Santiago <santi.ari.pani@outlook.com>
2021-12-25 15:40:35 +0100

    rdfa

--- assets/js/script.js
+++ assets/js/script.js
@@ -105,44 +105,44 @@ $(function() {
 
     var resta1 = Math.floor((fechaLimite1 - fechaHoy) / (1000 * 3600 * 24));
     if (resta1 >= 0 && resta1 <= 7) {
-        document.getElementById("b1").innerHTML = "Promoción válida, quedan " + resta1 + " día/s";
+        document.getElementById("b1").innerHTML = "Promoción válida, quedan " + resta1 + " día/s <br> <link property='availability' href='https://schema.org/InStock' />";
         document.getElementById("b1").style.backgroundColor = "orange";
 
     } else if (resta1 < 0) {
-        document.getElementById("b1").innerHTML = "Promoción no válida";
+        document.getElementById("b1").innerHTML = "Promoción no válida <br> <link property='availability' href='https://schema.org/OutOfStock' />";
         document.getElementById("b1").style.backgroundColor = "red";
 
     } else {
-        document.getElementById("b1").innerHTML = "Promoción válida, quedan " + resta1 + " día/s";
+        document.getElementById("b1").innerHTML = "Promoción válida, quedan " + resta1 + " día/s <br> <link property='availability' href='https://schema.org/InStock' />";
     }
 
 
 
     var resta2 = Math.floor((fechaLimite2 - fechaHoy) / (1000 * 3600 * 24));
     if (resta2 >= 0 && resta2 <= 7) {
-        document.getElementById("b2").innerHTML = "Promoción válida, quedan " + resta2 + " día/s";
+        document.getElementById("b2").innerHTML = "Promoción válida, quedan " + resta2 + " día/s <br> <link property='availability' href='https://schema.org/InStock' />";
         document.getElementById("b2").style.backgroundColor = "orange";
 
     } else if (resta2 < 0) {
-        document.getElementById("b2").innerHTML = "Promoción no válida";
+        document.getElementById("b2").innerHTML = "Promoción no válida <br> <link property='availability' href='https://schema.org/OutOfStock' />";
         document.getElementById("b2").style.backgroundColor = "red";
 
     } else {
-        document.getElementById("b2").innerHTML = "Promoción válida, quedan " + resta2 + " día/s";
+        document.getElementById("b2").innerHTML = "Promoción válida, quedan " + resta2 + " día/s <br> <link property='availability' href='https://schema.org/InStock' />";
     }
 
 
 
     var resta3 = Math.floor((fechaLimite3 - fechaHoy) / (1000 * 3600 * 24));
     if (resta3 >= 0 && resta3 <= 7) {
-        document.getElementById("b3").innerHTML = "Promoción válida, quedan " + resta3 + " día/s";
+        document.getElementById("b3").innerHTML = "Promoción válida, quedan " + resta3 + " día/s <br> <link property='availability' href='https://schema.org/InStock' />";
         document.getElementById("b3").style.backgroundColor = "orange";
 
     } else if (resta3 < 0) {
-        document.getElementById("b3").innerHTML = "Promoción no válida";
+        document.getElementById("b3").innerHTML = "Promoción no válida <br> <link property='availability' href='https://schema.org/OutOfStock' />";
         document.getElementById("b3").style.backgroundColor = "red";
 
     } else {
-        document.getElementById("b3").innerHTML = "Promoción válida, quedan " + resta3 + " día/s";
+        document.getElementById("b3").innerHTML = "Promoción válida, quedan " + resta3 + " día/s <br> <link property='availability' href='https://schema.org/InStock' />";
     }
 });
